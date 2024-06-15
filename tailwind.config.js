a842b6766ec7acc7e15f5a4a7a434a12c69cf773
Amirhossein <4mirh03einhashemi@gmail.com>
2024-06-15 19:53:25 +0330

    add fontfamily and colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,28 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    colors: {
+      transparent: "transparent",
+      current: "currentColor",
+      primary: {
+        blue: {
+          1: "hsl(213, 96%, 18%)",
+          2: "hsl(243, 100%, 62%)",
+          3: "hsl(228, 100%, 84%)",
+          4: "hsl(206, 94%, 87%)",
+        },
+        red: "hsl(354, 84%, 57%)",
+      },
+      neutral: {
+        gray: {
+          1: "hsl(231, 11%, 63%)",
+          2: "hsl(229, 24%, 87%)",
+        },
+        Magnolia: "hsl(217, 100%, 97%)",
+        Alabaster: "hsl(231, 100%, 99%)",
+        white: "hsl(0, 0%, 100%)",
+      },
+    },
     extend: {},
   },
   plugins: [],
