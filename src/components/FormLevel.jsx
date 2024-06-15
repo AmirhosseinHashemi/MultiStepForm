75beca7fad15ae5c40860a54720d5ed96ba30833
Amirhossein <4mirh03einhashemi@gmail.com>
2024-06-15 22:01:02 +0330

    create formlevel steps

--- src/components/FormLevel.jsx
+++ src/components/FormLevel.jsx
@@ -1,6 +1,29 @@
 function FormLevel() {
   return (
-    <aside className="relative h-[172px] bg-[url('/images/bg-sidebar-mobile.svg')] bg-top bg-no-repeat"></aside>
+    <aside className="flex h-[172px] justify-center bg-[url('/images/bg-sidebar-mobile.svg')] bg-top bg-no-repeat">
+      <ul className="mt-8 flex gap-4">
+        <li>
+          <button className="h-8 w-8 rounded-full border border-primary-blue-4 text-sm font-medium text-neutral-white">
+            1
+          </button>
+        </li>
+        <li>
+          <button className="h-8 w-8 rounded-full border border-primary-blue-4 text-sm font-medium text-neutral-white">
+            2
+          </button>
+        </li>
+        <li>
+          <button className="h-8 w-8 rounded-full border border-primary-blue-4 text-sm font-medium text-neutral-white">
+            3
+          </button>
+        </li>
+        <li>
+          <button className="h-8 w-8 rounded-full border border-primary-blue-4 text-sm font-medium text-neutral-white">
+            4
+          </button>
+        </li>
+      </ul>
+    </aside>
   );
 }
 
