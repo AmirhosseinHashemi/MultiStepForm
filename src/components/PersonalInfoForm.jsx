2bbc95c0b28f4f936f082d0a914ebf4eee2ae9d9
Amirhossein <4mirh03einhashemi@gmail.com>
2024-06-23 01:07:05 +0330

    implement form validation

--- src/components/PersonalInfoForm.jsx
+++ src/components/PersonalInfoForm.jsx
@@ -1,11 +1,27 @@
+import { useForm } from "react-hook-form";
+
 function PersonalInfoForm() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  function submit(data) {
+    console.log(data);
+  }
+
   return (
     <section className="mt-[-72px] rounded-lg bg-neutral-white px-6 py-8">
       <h2 className="text-2xl font-bold text-primary-blue-1">Personal info</h2>
       <p className="mt-2 text-[16px] font-normal text-neutral-gray-1">
         Please provide your name, email, address, and phone number.
       </p>
-      <form action="" className="mt-3 flex flex-col gap-4">
+      <form
+        action=""
+        className="mt-3 flex flex-col gap-4"
+        onSubmit={handleSubmit(submit)}
+      >
         <div className="flex flex-col">
           <label
             htmlFor="name"
@@ -19,12 +35,24 @@ function PersonalInfoForm() {
             name="name"
             id="name"
             placeholder="e.g. Stephen King"
+            {...register("name", {
+              required: "This field is required",
+              pattern: {
+                value: /^[A-Z][a-zA-Z]*\s[A-Z][a-zA-Z]*$/,
+                message: "You should fill input like example",
+              },
+            })}
           />
+          {errors.name && (
+            <p className="mt-1 text-xs font-bold text-primary-red">
+              {errors.name.message}
+            </p>
+          )}
         </div>
 
         <div className="flex flex-col">
           <label
-            htmlFor="name"
+            htmlFor="email"
             className="text-sm font-medium capitalize text-primary-blue-1"
           >
             Email address
@@ -35,12 +63,24 @@ function PersonalInfoForm() {
             name="email"
             id="email"
             placeholder="e.g. stephenking@lorem.com"
+            {...register("email", {
+              required: "This field is required",
+              pattern: {
+                value: /^[a-zA-Z0-9._%+-]{6,30}@gmail\.com$/,
+                message: "You should fill input like example",
+              },
+            })}
           />
+          {errors.email && (
+            <p className="mt-1 text-xs font-bold text-primary-red">
+              {errors.email.message}
+            </p>
+          )}
         </div>
 
         <div className="flex flex-col">
           <label
-            htmlFor="name"
+            htmlFor="phone"
             className="text-sm font-medium capitalize text-primary-blue-1"
           >
             Phone Number
@@ -51,7 +91,23 @@ function PersonalInfoForm() {
             name="phone"
             id="phone"
             placeholder="e.g. +1 234 567 890"
+            {...register("phone", {
+              required: "This field is required",
+              pattern: {
+                value:
+                  /^\s*(?:\+?(\d{1,3}))?([-. (]*(\d{3})[-. )]*)?((\d{3})[-. ]*(\d{2,4})(?:[-.x ]*(\d+))?)\s*$/,
+                message: "Your phone number format is invalid",
+              },
+            })}
           />
+          {errors.phone && (
+            <p className="mt-1 text-xs font-bold text-primary-red">
+              {errors.phone.message}
+            </p>
+          )}
+          <button hidden={true} type="submit">
+            Submit
+          </button>
         </div>
       </form>
     </section>
