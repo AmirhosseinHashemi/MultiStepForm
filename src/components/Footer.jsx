3c04a6622141b36116bf4184ac040cd357409aac
Amirhossein <4mirh03einhashemi@gmail.com>
2024-06-23 20:50:02 +0330

    display buttons conditionaly based on form level

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,14 @@
+import useFormContext from "../hooks/useFormContext";
 import Button from "./Button";
 
 function Footer() {
+  const { level } = useFormContext();
+
   return (
     <footer className="fixed bottom-0 left-0 right-0 mx-auto flex max-w-[375px] flex-row-reverse justify-between bg-neutral-white p-4">
-      <Button type="next">Next Step</Button>
+      {level === 4 && <Button type="confirm">Confirm</Button>}
+      {level > 0 && level < 4 && <Button type="next">Next Step</Button>}
+      {level > 1 && <Button type="back">Go Back</Button>}
     </footer>
   );
 }
