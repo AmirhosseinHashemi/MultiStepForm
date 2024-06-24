d686439f455ff5032b58b72d890d43b424f243df
Amirhossein <4mirh03einhashemi@gmail.com>
2024-06-24 18:37:08 +0330

    ♻ refactor AddOns
    replace useState with custom hook and sync code with new custom hook

--- src/components/AddOns.jsx
+++ src/components/AddOns.jsx
@@ -1,7 +1,17 @@
+import { useForm } from "react-hook-form";
+
 import { ADD_ONS } from "../utils/config";
+import useFormDispatchContext from "../hooks/useFormDispatchContext";
 import AddOnsItem from "./AddOnsItem";
 
 function AddOns() {
+  const { register, handleSubmit } = useForm();
+  const dispatch = useFormDispatchContext();
+
+  function submitForm(data) {
+    dispatch({ type: "add_ons", payload: data });
+  }
+
   return (
     <section className="mt-[-72px] rounded-lg bg-neutral-white px-6 py-8 shadow-xl">
       <h2 className="text-2xl font-bold text-primary-blue-1">Pick add-ons</h2>
@@ -9,9 +19,11 @@ function AddOns() {
         Add-ons help enhance your gaming experience.
       </p>
 
-      {ADD_ONS.map((option) => (
-        <AddOnsItem key={option.id} option={option} />
-      ))}
+      <form action="" onSubmit={handleSubmit(submitForm)}>
+        {ADD_ONS.map((option) => (
+          <AddOnsItem key={option.id} option={option} register={register} />
+        ))}
+      </form>
     </section>
   );
 }
--- src/components/AddOnsItem.jsx
+++ src/components/AddOnsItem.jsx
@@ -1,16 +1,20 @@
-function AddOnsItem({ option }) {
-  // price will handle later...
-  // inputs are not in control yet
+function AddOnsItem({ option, register }) {
   const { name, id, description } = option;
+
   return (
     <div
       key={id}
       className="mb-3 grid grid-cols-[auto_1fr_auto] items-center gap-3 rounded-lg border border-neutral-gray-2 p-4"
     >
-      <input
-        className="h-5 w-5 appearance-none rounded border border-neutral-gray-2 bg-transparent bg-[url('/images/icon-checkmark.svg')] checked:border-none checked:bg-primary-blue-2 checked:bg-center checked:bg-no-repeat"
-        type="checkbox"
-      />
+      <label htmlFor={name} className="h-5 w-5">
+        <input
+          className="size-full appearance-none rounded border border-neutral-gray-2 bg-transparent bg-[url('/images/icon-checkmark.svg')] checked:border-none checked:bg-primary-blue-2 checked:bg-center checked:bg-no-repeat"
+          type="checkbox"
+          name={name}
+          id={name}
+          {...register(name)}
+        />
+      </label>
       <div>
         <h3 className="text-sm font-bold -tracking-wide text-primary-blue-1">
           {name}
